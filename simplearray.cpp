539269bc9e709bdf97905dce989aebf77600eb2d
Nijo Luca <nijoluca125@gmail.com>
2020-09-11 10:49:35 +0000

    new commit

---
+++ simplearray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int arr[5];
+  arr[0]=10;
+  arr[2]=-23;
+  arr[3/2]=2;
+  arr[2*2]=3;
+  cout<<arr[0]<<" "<<arr[2]<<" "<<arr[4];
+return 0;
+}
