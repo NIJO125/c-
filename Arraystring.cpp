963e944e71743b7143ad2a74d4e317d93431f094
Nijo Luca <nijoluca125@gmail.com>
2020-09-11 10:56:05 +0000

    commit on 9/11/2020

---
+++ Arraystring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+  string str="I love you c++";
+  string str1="I like to program in c++";
+  std::cout<<str<<endl<<str1<<endl<<str + str1;
+  return 0;
+}
